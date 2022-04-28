3f50d5734d7f70f52c5ea9ed38414dc9cf02addd
andrew-anchor <andrew.wu@anchor-meta.com>
2022-04-28 15:47:04 +0800

    feat: react app init success

--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <h1>Here is Andrew speaking...</h1>
+        <h2>This is the Azure SWA react app V1.</h2>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
